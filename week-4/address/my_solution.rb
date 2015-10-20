b617c4e14270ab192d9a2a6d3fa784b19aaa85de
luisplaz <luisplaz@hotmail.com>
2015-10-20 13:05:36 -0400

    Add Recent Changes

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,17 @@
+# Format an Address
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def make_address(street, city, state, zip)
+
+	address_street = street.to_s
+	address_city = city
+	address_state = state
+	adress_zip = zip.to_s
+
+	puts 'You live at ' + address_street + ', in the beautiful city of ' + address_city + ', ' + address_state + '. Your zip is ' + adress_zip + '.'
+
+end
