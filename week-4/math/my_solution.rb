3ae9a5a007208ab199d64896fdff4e1b8ecc2ae1
luisplaz <luisplaz@hotmail.com>
2015-10-20 14:39:09 -0400

    Make Changes Return Solution

--- week-4/math/my_solution.rb
+++ week-4/math/my_solution.rb
@@ -8,25 +8,22 @@
 def add(num_1, num_2)
  #your code here
 
-puts num_1+num_2
+	p num_1+num_2
 
 end
 
 def subtract(num_1, num_2)
  #your code here
- first_integer = num_1.to_i
- second_integer = num_2.to_i
- subt = first_integer - second_integer
- puts subt
+	p num_1 - num_2
 
 end
 
 def multiply(num_1, num_2)
  #your code here
- 	puts num_1 * num_2
+ 	p num_1 * num_2
 end
 
 def divide(num_1, num_2)
  #your code here
- 	puts num_1.to_f / num_2.to_f
+ 	p num_1.to_f / num_2.to_f
 end
